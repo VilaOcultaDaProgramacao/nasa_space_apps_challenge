1b3ce5989a87f0908d2bb8f6ceee80700c48983d
Kevin Oliveira <kevinoliveiraofficial@users.noreply.github.com>
2019-10-22 03:06:13 -0300

    Removed All

--- front_end/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://2c41e213.ngrok.io"
-});
-
-export default api;
--- front_end/src/store/modules/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import auth from "./auth/reducer";
-import user from "./user/reducer";
-import spot from "./Spot/reducer";
-
-export default combineReducers({ auth, user, spot });
